a252b986818d6e192fc921176339d23ed8e347a4
luisyet <sulcaluis151999@gmail.com>
2024-01-13 07:20:56 -0500

    calculadoraApp

--- calculadoraApp/src/app/app.component.ts
+++ calculadoraApp/src/app/app.component.ts
@@ -6,5 +6,17 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'calculadoraApp';
+  title = 'App Calculadora';  //las propiedades tienen que estar inicializadas o definida su asignacion en un constructor sino error
+
+  numero1: number = 0;
+  numero2: number = 0;
+  resultado: number = 0;
+
+  sumar(){
+    this.resultado = this.numero1 + this.numero2;
+  }
+
+  restar(){
+    this.resultado = this.numero1 - this.numero2;
+  }
 }
