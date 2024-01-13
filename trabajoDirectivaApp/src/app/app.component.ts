5f96a01051c6d52585d55507cf342cd181a3a8fb
luisyet <sulcaluis151999@gmail.com>
2024-01-13 10:09:05 -0500

    directivas ngIf y ngFor

--- trabajoDirectivaApp/src/app/app.component.ts
+++ trabajoDirectivaApp/src/app/app.component.ts
@@ -6,5 +6,27 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'trabajoDirectivaApp';
+  title = 'Registro de Usuarios';
+
+  apellido: string = "";
+
+  mensaje = "Usuarios registrado con éxito";
+  registrado = false;
+
+  // entradas: object[];
+  // entradas: any;
+  entradas: Array<{titulo: string}>
+
+  constructor(){
+    this.entradas = [
+      {titulo: 'PHP no está muerto'},
+      {titulo: 'Java presenta más de 20 años'},
+      {titulo: 'Python cada vez más presente'}
+    ]
+  }
+
+  registrarUsuario(){
+    this.registrado = true;
+  }
+
 }
