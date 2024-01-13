7c7edb428a77d5cc248c65bd89a8d7fb5ba154b1
luisyet <sulcaluis151999@gmail.com>
2024-01-13 11:55:23 -0500

    formulario registro empleados

--- app-empleados/src/app/app.component.ts
+++ app-empleados/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Empleado } from './empleado.model';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,21 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'app-empleados';
+  titulo = 'Listado de Empleados';
+
+  empleados: Empleado[] = [
+    new Empleado("Luis", "U", "Practicante", 1200),
+    new Empleado("Pedro", "P", "Arquitecto", 2400),
+    new Empleado("Carlo", "T", "Desarrollador", 200),
+  ];
+
+  cuadroNombre: string = "";
+  cuadroApellido: string = "";
+  cuadroCargo: string = "";
+  cuadroSalario: number = 0;
+
+  agregarEmpleado(){
+    let nuevoEmpleado = new Empleado(this.cuadroNombre, this.cuadroApellido, this.cuadroCargo, this.cuadroSalario);
+    this.empleados.push(nuevoEmpleado);
+  }
 }
