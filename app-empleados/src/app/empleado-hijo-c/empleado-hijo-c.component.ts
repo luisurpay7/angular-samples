f0f4301478be2735ff9c5e52d4ea134d21a28444
luisyet <sulcaluis151999@gmail.com>
2024-01-13 19:19:04 -0500

    comunicacion entre componentes @Input

---
+++ app-empleados/src/app/empleado-hijo-c/empleado-hijo-c.component.ts
@@ -0,0 +1,15 @@
+import { Component, Input } from '@angular/core';
+import { Empleado } from '../empleado.model';
+
+@Component({
+  selector: 'app-empleado-hijo-c',
+  templateUrl: './empleado-hijo-c.component.html',
+  styleUrl: './empleado-hijo-c.component.css'
+})
+export class EmpleadoHijoCComponent {
+
+  @Input() empleadoDeLista: Empleado;
+  @Input() indice: number;
+
+
+}
