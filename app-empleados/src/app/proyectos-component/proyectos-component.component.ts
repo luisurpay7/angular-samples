1f9b50c88a8d7d2387077379b61175c1bef73681
luisyet <sulcaluis151999@gmail.com>
2024-01-20 11:03:12 -0500

    navigate with button

--- app-empleados/src/app/proyectos-component/proyectos-component.component.ts
+++ app-empleados/src/app/proyectos-component/proyectos-component.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ServicioEmpleadosService } from '../servicio-empleados.service';
+import { EmpleadoService } from '../empleados.service';
+import { Empleado } from '../empleado.model';
 
 @Component({
   selector: 'app-proyectos-component',
   templateUrl: './proyectos-component.component.html',
   styleUrl: './proyectos-component.component.css'
 })
-export class ProyectosComponentComponent {
+export class ProyectosComponentComponent implements OnInit{
 
+  constructor(private router: Router, private empleadosService: EmpleadoService){}
+
+  ngOnInit(): void {
+    this.empleados = this.empleadosService.empleados;
+  }
+
+  empleados: Empleado[] = [];
+
+  cuadroNombre: string = "";
+  cuadroApellido: string = "";
+  cuadroCargo: string = "";
+  cuadroSalario: number = 0;
+
+  agregarEmpleado(){
+    let nuevoEmpleado = new Empleado(this.cuadroNombre, this.cuadroApellido, this.cuadroCargo, this.cuadroSalario);
+    // this.miServicio.muestarMensaje("Nombre del empleado: " + nuevoEmpleado.nombre);
+    this.empleadosService.agregarEmpleadoServicio(nuevoEmpleado);
+    this.router.navigate(['']);
+  }
+
+  volverHome(){
+    this.router.navigate(['']);
+  }
 }
