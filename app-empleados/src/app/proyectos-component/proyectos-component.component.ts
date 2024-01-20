15c3a1f553abafd0b73611b169bdbe46118b9d41
luisyet <sulcaluis151999@gmail.com>
2024-01-20 10:04:51 -0500

    add components for pages

---
+++ app-empleados/src/app/proyectos-component/proyectos-component.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-proyectos-component',
+  templateUrl: './proyectos-component.component.html',
+  styleUrl: './proyectos-component.component.css'
+})
+export class ProyectosComponentComponent {
+
+}
