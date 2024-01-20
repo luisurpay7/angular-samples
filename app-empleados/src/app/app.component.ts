c0270d5330785c88c84e1815fb38559c67d326a8
luisyet <sulcaluis151999@gmail.com>
2024-01-20 10:26:20 -0500

    add routes

--- app-empleados/src/app/app.component.ts
+++ app-empleados/src/app/app.component.ts
@@ -1,33 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import { Empleado } from './empleado.model';
-import { ServicioEmpleadosService } from './servicio-empleados.service';
-import { EmpleadoService } from './empleados.service';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent implements OnInit {
-  titulo = 'Listado de Empleados';
+export class AppComponent {
 
-  constructor(private miServicio: ServicioEmpleadosService, private empleadosService: EmpleadoService){
-    // this.empleados = this.empleadosService.empleados;
-  }
-  ngOnInit(): void {
-    this.empleados = this.empleadosService.empleados;
-  }
-
-  empleados: Empleado[] = [];
-
-  cuadroNombre: string = "";
-  cuadroApellido: string = "";
-  cuadroCargo: string = "";
-  cuadroSalario: number = 0;
-
-  agregarEmpleado(){
-    let nuevoEmpleado = new Empleado(this.cuadroNombre, this.cuadroApellido, this.cuadroCargo, this.cuadroSalario);
-    // this.miServicio.muestarMensaje("Nombre del empleado: " + nuevoEmpleado.nombre);
-    this.empleadosService.agregarEmpleadoServicio(nuevoEmpleado);
-  }
 }
--- app-empleados/src/app/home-component/home-component.component.ts
+++ app-empleados/src/app/home-component/home-component.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Empleado } from '../empleado.model';
+import { ServicioEmpleadosService } from '../servicio-empleados.service';
+import { EmpleadoService } from '../empleados.service';
 
 @Component({
   selector: 'app-home-component',
   templateUrl: './home-component.component.html',
   styleUrl: './home-component.component.css'
 })
-export class HomeComponentComponent {
+export class HomeComponentComponent implements OnInit {
+  titulo = 'Listado de Empleados';
 
+  constructor(private miServicio: ServicioEmpleadosService, private empleadosService: EmpleadoService){
+    // this.empleados = this.empleadosService.empleados;
+  }
+
+  ngOnInit(): void {
+    this.empleados = this.empleadosService.empleados;
+  }
+
+  empleados: Empleado[] = [];
+
+  cuadroNombre: string = "";
+  cuadroApellido: string = "";
+  cuadroCargo: string = "";
+  cuadroSalario: number = 0;
+
+  agregarEmpleado(){
+    let nuevoEmpleado = new Empleado(this.cuadroNombre, this.cuadroApellido, this.cuadroCargo, this.cuadroSalario);
+    // this.miServicio.muestarMensaje("Nombre del empleado: " + nuevoEmpleado.nombre);
+    this.empleadosService.agregarEmpleadoServicio(nuevoEmpleado);
+  }
 }
