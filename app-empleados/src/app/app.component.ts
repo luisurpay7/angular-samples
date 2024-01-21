31f65790e181dad9dcd0aff588f430374134c23a
luisyet <sulcaluis151999@gmail.com>
2024-01-21 10:10:48 -0500

    firebase config

--- app-empleados/src/app/app.component.ts
+++ app-empleados/src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+// import { initializeApp } from 'firebase/app';
+import firebase from 'firebase/compat/app';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
 
+  ngOnInit(): void {
+    firebase.initializeApp({
+      apiKey: "AIzaSyCBp41yDa3SYsNYnatUPbBpPF2yqMFMKLo",
+      authDomain: "clientesangular-a5570.firebaseapp.com",
+    })
+  }
 }
---
+++ app-empleados/src/app/login/login.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from "@angular/core";
+import { Router } from "@angular/router";
+
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth'
+
+@Injectable()
+export class LoginService {
+
+  constructor(private router: Router){}
+
+  token: string;
+
+  login(email: string, password: string){
+    firebase.auth().signInWithEmailAndPassword(email, password)   //return promise
+      .then(response => {
+        firebase.auth().currentUser?.getIdToken().then(
+          token => {
+            this.token = token;
+            this.router.navigate(['/']);
+          }
+        )
+      });
+
+  }
+
+  getIdToken(){
+    return this.token;
+  }
+
+}
