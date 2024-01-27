5704bd809ea4338e70b40b5d8c86926f428c600e
luisyet <sulcaluis151999@gmail.com>
2024-01-27 17:57:21 -0500

    changeDetection

--- changeDetection/src/app/app.component.ts
+++ changeDetection/src/app/app.component.ts
@@ -7,4 +7,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'changeDetection';
+  data = 'Luis';
+
+  changeData(){
+    this.data = 'Angel';
+  }
+
+  keyup(){}
 }
---
+++ changeDetection/src/app/child/child.component.ts
@@ -0,0 +1,14 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-child',
+  templateUrl: './child.component.html',
+  styleUrl: './child.component.css'
+})
+export class ChildComponent {
+  @Input() data: string = '';
+
+  isCalled(){
+    console.log('Child is called');
+  }
+}
